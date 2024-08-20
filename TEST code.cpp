a8834ba1f360e5e561662d1cb7fb74b26ceea8ba
Tanbir Hasan Jewel <141549622+Githbir@users.noreply.github.com>
2024-08-20 23:39:08 +0600

    Add files via upload

---
+++ TEST code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define e cout<<'\n';
+#define int long long
+int t=1,n,m,k,x;
+void Solve()
+{
+   cin>>n;
+   vector<int>a(n);
+   for(int i=0;i<n;i++)cin>>a[i];
+
+   //  ?cout<<"YES":cout<<"NO";
+}
+int32_t main()
+{
+   ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+   cin>>t;
+   while(t--)Solve();
+}
